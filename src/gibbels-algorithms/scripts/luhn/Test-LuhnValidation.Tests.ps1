d6a3e763b03d3fd3feb5cfb58dec15133bda5ae3
Taylor Gibb <Taylor Gibb>
2016-09-13 19:29:08 +0200

    Refactor tests

--- src/gibbels-algorithms/scripts/luhn/Test-LuhnValidation.Tests.ps1
+++ src/gibbels-algorithms/scripts/luhn/Test-LuhnValidation.Tests.ps1
@@ -1,9 +1,9 @@
 Import-Module $([System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSCommandPath, '..\..\..\gibbels-algorithms.psd1')));
 
-Describe "Luhn Validation" {
-  Context "Given VISA credit card number" {
+Describe "Test-LuhnValidation" {
+  Context "Given a credit card number" {
       $result = Test-LuhnValidation -Number 4288667983082851
-      It "Should pass Luhn Validation" {
+      It "Should pass validation" {
           $result | Should Be $true
       }
     }
