d3efc35f2ad7ca265f6a6cefedee2312d2956fb9
Taylor Gibb <Taylor Gibb>
2016-11-10 08:20:28 +0200

    Updating build

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ param(
     [string[]]$Task = 'default'
 )
 
+Install-Module -Name PoweShellGet -Scope CurrentUser
 
 if (!(Get-Module -Name psake -ListAvailable)) { 
     Install-Module -Name psake -Scope CurrentUser 
