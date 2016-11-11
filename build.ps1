97b2eae0474c507229740662500925e51ec2d0d6
Taylor Gibb <Taylor Gibb>
2016-11-11 02:54:13 +0200

    Removing psake

--- build.ps1
+++ build.ps1
@@ -1,26 +1,89 @@
 param(
-    [string[]]$Task = 'default'
+    [string[]]$Tasks
 )
 
-$policy = Get-PSRepository -Name "PSGallery" | Select-Object -ExpandProperty "InstallationPolicy"
-if($policy -ne "Trusted") {
-    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+function Install-Dependencies
+{
+    $policy = Get-PSRepository -Name "PSGallery" | Select-Object -ExpandProperty "InstallationPolicy"
+    if($policy -ne "Trusted") {
+        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    }
+    
+    if (!(Get-Module -Name psake -ListAvailable)) { 
+        Install-Module -Name psake -Scope CurrentUser 
+    }
+    
+    if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { 
+        Install-Module -Name PSScriptAnalyzer -Scope CurrentUser 
+    }
+    
+    if (!(Get-Module -Name Pester -ListAvailable)) { 
+        Install-Module -Name Pester -Scope CurrentUser 
+    }
+    
+    if (!(Get-Module -Name PSDeploy -ListAvailable)) { 
+        Install-Module -Name PSDeploy -Scope CurrentUser 
+    }
 }
 
-if (!(Get-Module -Name psake -ListAvailable)) { 
-    Install-Module -Name psake -Scope CurrentUser 
+function Analyze-Scripts
+{
+    param(
+        [string]$Path = "$PSScriptRoot\src\"
+    )
+    Write-Output "Analyzing Scripts..."
+    $result = Invoke-ScriptAnalyzer -Path $Path -Severity @('Error', 'Warning') -Recurse
+    if ($result) {
+       $result | Format-Table  
+       Write-Error -Message 'One or more Script Analyzer errors or warnings were found.'   
+       EXIT 1     
+    }
 }
 
-if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { 
-    Install-Module -Name PSScriptAnalyzer -Scope CurrentUser 
+function Run-Tests
+{
+    param(
+        [string]$Path = "$PSScriptRoot\src\"
+    )
+    Write-Output "Running Pester Tests..."
+    Invoke-Pester -Path $Path -Quiet
 }
 
-if (!(Get-Module -Name Pester -ListAvailable)) { 
-    Install-Module -Name Pester -Scope CurrentUser 
+function Deploy-Modules
+{
+    try {
+       if($(Get-GitCommitMessage) -like "[deploy]") {
+          Write-Output "Deploying Modules..."
+          Invoke-PSDeploy -Force 
+       }
+       else {
+          Write-Output "Skipping Deploy..."
+       }
+    }
+    catch {
+       Write-Output $_.Exception.Message
+      # EXIT 1
+    }
 }
 
-if (!(Get-Module -Name PSDeploy -ListAvailable)) { 
-    Install-Module -Name PSDeploy -Scope CurrentUser 
+function Get-GitCommitMessage
+{
+    git log -1 --pretty=%B
 }
 
-Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task
+Install-Dependencies
+
+foreach($task in $Tasks){
+    switch($task)
+    {
+        "analyze" {
+            Analyze-Scripts
+        }
+        "test" {
+            Run-Tests
+        }
+        "release" {
+            Deploy-Modules
+        }
+    }
+}
